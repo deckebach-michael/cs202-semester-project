66d2851cf472eb84db2828e6c2bfcd443a6b0ace
TurngrenThomas <lvthomas01@gmail.com>
2021-12-05 20:24:49 -0800

    Doxygen HTML and Latex

---
+++ docs/html/search/all_3.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['getbitdepth_0',['getBitDepth',['../classWav.html#a964321451ea52b3f957566bc567d577b',1,'Wav']]],
+  ['getchannels_1',['getChannels',['../classWav.html#a2ccfb5597ec5b0726f346e61071c4757',1,'Wav']]],
+  ['getfilename_2',['getFileName',['../classMenu.html#aba974a41ba50d42affe35c4d389099d5',1,'Menu']]],
+  ['getinputfile_3',['getInputFile',['../classWav.html#ad0bc9e617555903a2531a1e7ad917d52',1,'Wav']]],
+  ['getoutputname_4',['getOutputName',['../classMenu.html#ad97c2e946830bfe590f3758f34a13ed3',1,'Menu']]],
+  ['getsample_5',['getSample',['../classChannel.html#ad72d7442f4baaba164a327845f812f0b',1,'Channel']]],
+  ['getsamplerate_6',['getSampleRate',['../classWav.html#a7859a484d7fd993db7344b8fc201d1da',1,'Wav']]]
+];
---
+++ docs/html/search/functions_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['read_0',['read',['../classWav.html#af6f22a6df929e9995ef866678f36486d',1,'Wav']]]
+];
